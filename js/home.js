c5e6546572c61bb313eb83dfc7d3999ba1a1f858
boubaSambare <boubasambare@yahoo.it>
2019-11-03 23:49:53 +0100

    add css

--- js/home.js
+++ js/home.js
@@ -2,43 +2,43 @@ var ALBUMS = [
     {
       title: 'Italian Karaoke',
       category: '#throwbackthursday',
-      img: 'https://i.scdn.co/image/1353990534aef10c946cf3a47865ac22471be5c4'
+      img: 'https://i.scdn.co/image/6d61103bd4043701d6cd230a02fa43d916c4f144'
     },
     
     {
       title: 'OOs italy',
       category: '#throwbackthursday',
-      img: 'https://i.scdn.co/image/1353990534aef10c946cf3a47865ac22471be5c4'
+      img: 'https://i.scdn.co/image/8c1643c6a03f950c193184e1e35de3969d311adc'
     },
     {
       title: 'Cooktails Hours',
       category: '#throwbackthursday',
-      img: 'https://i.scdn.co/image/1353990534aef10c946cf3a47865ac22471be5c4'
+      img: 'https://i.scdn.co/image/ab67706f000000025af1070c80cd50dbbb4cfa19'
     },
     {
       title: 'I Love my 90\'s Hip Hop',
       category: '#throwbackthursday',
-      img: 'https://i.scdn.co/image/1353990534aef10c946cf3a47865ac22471be5c4'
+      img: 'https://pl.scdn.co/images/pl/default/7f90694e6e46b9bd702b639c71945bfc207ef4b3'
     },
     {
       title: 'Top-50 Italy',
       category: 'clasific',
-      img: 'https://i.scdn.co/image/1353990534aef10c946cf3a47865ac22471be5c4'
+      img: 'https://charts-images.scdn.co/REGIONAL_GLOBAL_DEFAULT.jpg'
     },
     {
       title: 'Top-50 Global',
       category: 'clasific',
-      img: '2-4'
+      img: 'https://charts-images.scdn.co/REGIONAL_GLOBAL_DEFAULT.jpg'
     },
     {
       title: 'Viral-50 Italy',
       category: 'clasific',
-      img: 'https://i.scdn.co/image/1353990534aef10c946cf3a47865ac22471be5c4'
+      img: 'https://charts-images.scdn.co/VIRAL_GLOBAL_DEFAULT.jpg'
     },
     {
       title: 'Viral-Global',
       category: 'clasific',
-      img: 'https://i.scdn.co/image/1353990534aef10c946cf3a47865ac22471be5c4'
+      img: 'https://charts-images.scdn.co/VIRAL_GLOBAL_DEFAULT.jpg'
     }
   ]
 var throwBackThursdayRow = _$('#throw-back-thursday')
@@ -75,7 +75,8 @@ var classificationRow = _$('#classification')
      colContainer.className = 'col-12 col-sm-6 col-lg-3 my-3'
      var albumCover = _$C('div')
      albumCover.className = 'cover-album'
-     albumCover.style.backgroundImage = album.img
+     albumCover.style.backgroundImage = 'url(' + album.img + ')'
+     albumCover.backgroundPosition = 'center'
      var iconLink = _$C('a')
      iconLink.href = '#'
      iconLink.className = 'text-white'
